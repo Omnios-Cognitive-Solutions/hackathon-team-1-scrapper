7cd8f06975a7c5b80c79aa93dfd9cc2d13da33c8
DidacElgueta <delgueta@omnios.ai>
2022-09-16 11:18:18 +0200

    add login function

--- src/linkedin_scrapper.py
+++ src/linkedin_scrapper.py
@@ -1,3 +1,5 @@
+import logging
+
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
@@ -15,9 +17,25 @@ class LinkedInScrapper(BaseSeleniumCrawler):
     def __init__(self, *args, **kwargs):
         url = self._url
         super().__init__(url, *args, **kwargs)
+        self._logger = logging.getLogger(__name__)
 
-    def login(self, username, password):
-        pass
+    def login(self, username: str, password: str) -> bool:
+        try:
+            elem = self._driver.find_element(By.XPATH, "/html/body/nav/div/a[2]")  # nopep8
+            elem.click()
+
+            elem = self._driver.find_element(By.ID, "username")
+            elem.send_keys(username)
+            elem = self._driver.find_element(By.ID, "password")
+            elem.send_keys(password)
+            elem = self._driver.find_element(By.XPATH, "/html/body/div/main/div[3]/div[1]/form/div[3]/button")  # nopep8
+            elem.click()
+
+        except Exception as e:
+            self._logger.error(e, exc_info=True)
+            return False
+
+        return True
 
     def get_profile(self, username):
         pass
