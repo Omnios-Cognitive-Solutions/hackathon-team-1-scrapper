462edf7d80c6971b7861243a1c9ab1179f2b3c17
Oriol Trujillo <otrujillo@omnios.ai>
2022-09-16 13:39:11 +0200

    Merged with didac's

--- src/linkedin_scrapper.py
+++ src/linkedin_scrapper.py
@@ -3,6 +3,8 @@ import logging
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+from src.linkedin_profile_scrapper import LinkedInProfileScrapper
+
 
 class LinkedInScrapper:
 
@@ -19,27 +21,20 @@ class LinkedInScrapper:
         self._driver = webdriver.Firefox(options=options)
 
     def login(self, username: str, password: str) -> bool:
-        try:
-            self._driver.get(self._base_url)
-            elem = self._driver.find_element(By.XPATH, "/html/body/nav/div/a[2]")
-            elem.click()
-            elem = self._driver.find_element(By.ID, "username")
-            elem.send_keys(username)
-            elem = self._driver.find_element(By.ID, "password")
-            elem.send_keys(password)
-            elem = self._driver.find_element(
-                By.XPATH, "/html/body/div/main/div[3]/div[1]/form/div[3]/button"
-            )
-            elem.click()
-        except Exception as e:
-            self._logger.error(e, exc_info=True)
-            return False
-
-        return True
+        self._driver.get(self._base_url)
+        elem = self._driver.find_element(By.XPATH, "/html/body/nav/div/a[2]")
+        elem.click()
+        elem = self._driver.find_element(By.ID, "username")
+        elem.send_keys(username)
+        elem = self._driver.find_element(By.ID, "password")
+        elem.send_keys(password)
+        elem = self._driver.find_element(
+            By.XPATH, "/html/body/div/main/div[3]/div[1]/form/div[3]/button"
+        )
+        elem.click()
 
     def close_driver(self) -> None:
         self._driver.close()
 
-    def get_profile(self, username: str):
-        basic_data = self.get_basic_info(username)
-        return basic_data
+    def get_profile(self, username: str) -> dict:
+        return LinkedInProfileScrapper(self._driver, username).get_data()
--- tests/test_linkedin_scrapper.py
+++ tests/test_linkedin_scrapper.py
@@ -1,16 +1,13 @@
-from src.linkedin_scrapper import LinkedInScrapper
 from src.config import config
+from src.linkedin_scrapper import LinkedInScrapper
 
-TEST_USERNAME = "NAREK"
+TEST_USERNAME = "dolores-fuertes-fernández-408743250"
 
-scrapper = LinkedInScrapper()
+scrapper = LinkedInScrapper(headless=True)
 
 
 def test_login():
-    scrapper.login(
-        config.get('LOGIN', 'username'),
-        config.get('LOGIN', 'password')
-    )
+    scrapper.login(config.get("LOGIN", "username"), config.get("LOGIN", "password"))
 
 
 def test_get_profile_basic_data():
